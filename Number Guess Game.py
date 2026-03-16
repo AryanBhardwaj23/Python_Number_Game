af482d745afa79327cf0e0d2241f942543597ec4
Aryan Bhardwaj <bhardwaj.2006aryan@gmail.com>
2026-03-16 23:15:51 +0530

    Update Number Guess Game.py

--- Number Guess Game.py
+++ Number Guess Game.py
@@ -4,7 +4,7 @@ to_guess=random.randint(1, 100)
 while True:
     try:
         num=int(input("Guess the number (b/w 1 and 100) : "))
-        if num<1 and num>100 :
+        if num<1 or num>100 :
             print("ENTER NUMBER WITHIN RANGE")
             continue
         if num>to_guess:
